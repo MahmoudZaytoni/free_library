74e05bdaaedc0bb4e60cad1ce0bcce14d18ac684
MahmoudZaytoni <mahmoudzaytoni2002@gmail.com>
2022-08-24 21:23:45 +0000

    Add Delete fuction and Cover for books

--- main.py
+++ main.py
@@ -2,16 +2,23 @@ import imp,os
 from flask import Flask, render_template
 from flask import request, redirect
 from flask import url_for
-
-
+from werkzeug.utils import secure_filename
+from flask import send_from_directory
 from database import get_db, query_table, insert_book, delete_by_id
 
-
 app = Flask(__name__)
+UPLOAD_FOLDER = "static/uploads"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key
+
 ## USER View
 @app.route("/")
 def homePage():
-    return render_template('index.html')
+    books = query_table('books')
+    return render_template('index.html', books=books)
+
+# @app.route("/<string>")
+# def book():
 
 @app.route("/login")
 def login():
@@ -33,13 +40,17 @@ def books(): # Display Books and Add
                     custom_css="admin",
                     book_data=book_data)
     else:
+        file = request.files['cover']
+        filename = secure_filename(file.filename)
         book_details = (
+            filename,
             request.form['title'],
             request.form['author'],
             request.form['description']
         )
         print (book_details)
         insert_book(book_details)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'] + "/images",filename))
         return redirect(url_for('books'))
 
 
@@ -48,10 +59,13 @@ def books(): # Display Books and Add
 def update_book(id):
     conn = get_db()
     if request.method == "POST":
+        cover = request.form['cover']
         title = request.form['title']
         author = request.form['author']
         description = request.form['description']
-        conn.execute(f"UPDATE books SET title='{title}',author='{author}', description_book='{description}' WHERE id={id}")
+        conn.execute(f"""UPDATE books
+                        SET cover='{cover}', title='{title}',author='{author}', description_book='{description}'
+                        WHERE id={id}""")
         conn.commit()
         conn.close()
     elif request.method == "GET":
@@ -63,6 +77,10 @@ def update_book(id):
                                 book_details=book_details)
     return redirect(url_for('books'))
 
+@app.route("/Admin/delete/<int:id>")
+def delete_book(id):
+    delete_by_id('books', id)
+    return redirect(url_for('books'))
 
 
 if __name__ == "__main__":
--- tables.sql
+++ tables.sql
@@ -6,6 +6,7 @@
 
 CREATE TABLE IF NOT EXISTS books (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
+    cover varchar(255) NOT NULL,
     title varchar(255) NOT NULL,
     author varchar(255) NOT NULL,
     description_book TEXT
